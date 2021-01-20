938c6a4d8c4672fffe2edbd5882857f1f8ed0921
sovanmarat <maratwp@mail.ru>
2021-01-21 00:37:11 +0300

    feat: added level, obj app, imd

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+
+const app = {
+    stateApp: 'Train',
+    stateStartButton: false,
+    stateBurgerMenu: false,
+    baseNum: '0',
+    level_pushups: 'Easy',
+    level_sitUps: 'Simply',
+    level_squats: 'Easy',
+    level_burpee: 'Easy',
+    state_pushups: true,
+    state_sitUps: true,
+    state_squats: false,
+    state_burpee: false,
+    start_pushups: 0,
+    start_sitUps: 0,
+    start_squats: 0,
+    start_burpee: 0,
+    day_pushups: 0,
+    day_sitUps: 0,
+    day_squats: 0,
+    day_burpee: 0,
+    coefficient: 1
+  };
+
+export default app;
---
+++ src/js/level.js
@@ -0,0 +1,35 @@
+import UI from './UI';
+import language from './lang';
+
+
+export default class Level extends UI {
+   constructor() {
+   super();
+   }
+
+  renderLevel(exercise, currentLevel) {
+
+
+//
+const boxLevel = document.querySelector(`.${exercise}BoxLevel`);
+
+const levelImpossible= UI.renderElement(boxLevel, 'div', null, ['class', `level levelImpossible Impossible_${exercise}`]);
+levelImpossible.textContent = language[language.appLang][4]; //Impossible
+
+const levelHard = UI.renderElement(boxLevel, 'div', null, ['class', `level levelHard Hard_${exercise}`]);
+levelHard.textContent = language[language.appLang][5]; //Hard
+
+const levelAverage = UI.renderElement(boxLevel, 'div', null, ['class', `level levelAverage Average_${exercise}`]);
+levelAverage.textContent = language[language.appLang][6]; //Average
+
+const levelSimply = UI.renderElement(boxLevel, 'div', null, ['class', `level levelSimply Simply_${exercise}`]);
+levelSimply.textContent = language[language.appLang][7]; //Simply
+
+const levelEasy = UI.renderElement(boxLevel, 'div', null, ['class', `level levelEasy Easy_${exercise}`]);
+levelEasy.textContent = language[language.appLang][8]; //Easy
+//
+const activeLevel = document.querySelector(`.${currentLevel}_${exercise}`);
+activeLevel.classList.add('activeLevel');
+
+  }
+  }
